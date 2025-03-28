b3c42e1ebcba2f01fc87790fa5a89f9716319e2b
Рамазанов Виталий Глебович <vitaliy@ramazanov.msk.ru>
2025-03-28 23:42:47 +0300

    BinarySearchFunc added

--- algorithms/numeric/binarySearch/binarySearch.go
+++ algorithms/numeric/binarySearch/binarySearch.go
@@ -22,3 +22,30 @@ func BinarySearch[T cmp.Ordered](array []T, target T) int {
 
 	return idx
 }
+
+// Binary Search is a search algorithm that finds the position of a target value in a sorted array.
+// This function accepts any type of arguments, but a user should provide compare function.
+// Compare function returns:
+// 0 if two arguments are equal
+// +1 if the left argument is greater
+// -1 if the right argument is greater
+func BinarySearchFunc[T any](array []T, target T, compare func(lhs, rhs T) int) int {
+	var idx int
+	l := 0
+	r := len(array)
+
+	for l <= r {
+		idx = (l + r) / 2
+		if compare(array[idx], target) == 0 {
+			break
+		}
+
+		if compare(array[idx], target) == 1 {
+			r = idx - 1
+		} else {
+			l = idx + 1
+		}
+	}
+
+	return idx
+}
--- algorithms/numeric/binarySearch/binarySearch_test.go
+++ algorithms/numeric/binarySearch/binarySearch_test.go
@@ -28,6 +28,46 @@ func TestBinarySearch(t *testing.T) {
 
 	// then
 	if !slices.Equal(resultsArray, arrayToSearch) {
-		t.Error("BinarySearch does not work properly")
+		t.Errorf("BinarySearch does not work properly\n expected: %v, got: %v\n", arrayToSearch, resultsArray)
+	}
+}
+
+// This test is a bit more sophisticated version of a previos Binary Search test.
+// We introduce TestType that does not conform to cmp.Ordered generic constraint and a compareFunc that
+// we will use in BinarySearchFunc as a comparator.
+// Otherwise, test logic is the same as in the test above.
+func TestBinarySearchFunc(t *testing.T) {
+	type TestType struct {
+		Val int
+	}
+
+	compareFunc := func(lhs, rhs TestType) int {
+		if lhs.Val == rhs.Val {
+			return 0
+		} else if lhs.Val > rhs.Val {
+			return +1
+		} else {
+			return -1
+		}
+	}
+
+	// given
+	testSize := rand.Intn(10000)
+	arrayToSearch := func() []TestType {
+		array := make([]TestType, testSize, testSize)
+		for i := 0; i < testSize; i++ {
+			array[i] = TestType{Val: i}
+		}
+		return array
+	}()
+	// when
+	resultsArray := make([]int, testSize, testSize)
+	for i := 0; i < testSize; i++ {
+		resultsArray[i] = BinarySearchFunc(arrayToSearch, TestType{Val: i}, compareFunc)
+	}
+
+	// then
+	if !slices.EqualFunc(resultsArray, arrayToSearch, func(i int, t TestType) bool { return i == t.Val }) {
+		t.Errorf("BinarySearch does not work properly\n expected: %v, got: %v\n", arrayToSearch, resultsArray)
 	}
 }
